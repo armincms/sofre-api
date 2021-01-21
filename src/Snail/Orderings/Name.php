7155ecfd9f6905a1a6be2c1644b877e366caa2d0
Ismail Zare <zarehesmaiel@gmail.com>
2021-01-21 11:26:03 +0330

    Added some orderings

---
+++ src/Snail/Orderings/Name.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Armincms\SofreApi\Snail\Orderings;
+
+use Illuminate\Http\Request;
+use Armincms\Snail\Orderings\Ordering;
+
+class Name extends Ordering
+{    
+}
---
+++ src/Snail/Orderings/Rating.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Armincms\SofreApi\Snail\Orderings;
+
+use Illuminate\Http\Request;
+use Armincms\Snail\Orderings\Ordering;
+
+class Rating extends Ordering
+{   
+    /**
+     * Apply the order to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $direction
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $direction)
+    {      
+        return $query->orderByMenuRating($direction);
+    } 
+}
